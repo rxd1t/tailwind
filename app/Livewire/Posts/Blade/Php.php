2a0ce4730576078f02bff5f3e06ca9a3daaa5f28
Raditya Abimanyu Budi Pratama <radityabp57@gmail.com>
2025-01-18 10:26:57 +0700

    proweb

---
+++ app/Livewire/Posts/Blade/Php.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Posts\Blade;
+
+use Livewire\Component;
+
+class Php extends Component
+{
+    public function render()
+    {
+        return view('livewire.posts.blade.php');
+    }
+}
